9198824eb15f6897c7499292d20e349f046abd44
FelixCaballero <fsuko06@gmail.com>
2024-11-27 11:19:57 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+const dropdowns = document.querySelectorAll('.dropdown');
+
+
+dropdowns.forEach(dropdown => {
+    dropdown.addEventListener('click', () => {
+        
+        dropdown.classList.toggle('open');
+    });
+});
